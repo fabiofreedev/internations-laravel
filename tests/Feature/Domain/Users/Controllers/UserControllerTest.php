0c2fec6b02e79882501481d70b23748105244908
Fabio De Simone <fabio.freedev@gmail.com>
2022-07-03 11:56:54 +0200

    Created failing test for UserController

---
+++ tests/Feature/Domain/Users/Controllers/UserControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Domain\Users\Controllers\UserController;
+use App\Domain\Users\User;
+use Laravel\Sanctum\Sanctum;
+
+it('tests users routes are protected from non-logged users', function ($method, $route, $id) {
+    $this->json($method, action([UserController::class, $route], $id))->assertUnauthorized();
+})->with([
+    ['GET', 'index', []],
+    ['POST', 'store', []],
+    ['DELETE', 'destroy', ['user' => 1]]
+]);
+
+it('tests a route accessible only for admin users is unauthorized for non-admin users', function () {
+    Sanctum::actingAs(
+        User::factory()->create(),
+    );
+
+    $this->getJson(action([UserController::class, 'index']))->assertUnauthorized();
+});
