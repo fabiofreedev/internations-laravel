94cba4c0629b0a204347af53d1d2454ecedf52d4
Fabio De Simone <fabio.freedev@gmail.com>
2022-07-03 12:06:21 +0200

    Created and registered middleware to protect admin routes

---
+++ app/Http/Middleware/AdminAuthenticated.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use function abort;
+
+class AdminAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    {
+        if (Auth::check() && Auth::user()?->isAdmin()) {
+            return $next($request);
+        }
+
+        abort(401);
+    }
+}
