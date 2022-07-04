78d981c4b6ae88ba65537db5e05012599ec0e4b9
Fabio De Simone <fabio.freedev@gmail.com>
2022-07-04 09:29:33 +0200

    Added method to remove user from a group Improved delete group method to block group deletion if there are users associated to that group

--- app/Domain/Users/Groups/Controllers/GroupController.php
+++ app/Domain/Users/Groups/Controllers/GroupController.php
@@ -8,6 +8,8 @@ use App\Domain\Users\Groups\Requests\GroupStoreRequest;
 use App\Domain\Users\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class GroupController extends Controller
@@ -34,27 +36,54 @@ class GroupController extends Controller
     /**
      * A validation request should be created to ensure thar the group exists
      *
-     * @param int $id
-     * @return \Illuminate\Http\Response
+     * @param Group $group
+     * @return JsonResponse
      */
-    public function destroy(int $id): \Illuminate\Http\Response
+    public function destroy(Group $group): JsonResponse
     {
-        Group::destroy($id);
+        if (!$group->users->isEmpty()) {
+            $usersCount = $group->users->count();
+            return \response()->json(['error' => "The group {$group->id} cannot be deleted because it contains $usersCount users"], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
 
-        return Response('', Response::HTTP_NO_CONTENT);
+        Group::destroy($group->id);
+
+        return \response()->json('', Response::HTTP_NO_CONTENT);
     }
 
     /**
      * @param AddUserRequest $request
      * @param Group $group
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
+     * @throws \JsonException
      */
-    public function addUser(AddUserRequest $request, Group $group): \Illuminate\Http\Response
+    public function addUser(AddUserRequest $request, Group $group): JsonResponse
     {
         $user = User::findOrFail($request->user_id);
 
+        if ($group->users->contains($user)) {
+            return \response()->json(['error' => "User {$user->id} is already part of the group {$group->id}"], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
         $group->users()->attach($user);
 
-        return \response("User {$user->id} added to group {$group->id}", Response::HTTP_OK);
+        return \response()->json(['success' => "User {$user->id} added to group {$group->id}"]);
+    }
+
+    /**
+     * @param Request $request
+     * @param Group $group
+     * @param User $user
+     * @return JsonResponse
+     */
+    public function removeUser(Request $request, Group $group, User $user): JsonResponse
+    {
+        if (!$group->users->contains($user)) {
+            return \response()->json(['error' => "User {$user->id} is not part of the group {$group->id}"], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $group->users()->detach($user);
+
+        return \response()->json(['success' => "User {$user->id} has been removed from the group {$group->id}"]);
     }
 }
